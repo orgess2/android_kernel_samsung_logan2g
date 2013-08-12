db934cf35292fb4b5293d92762ff189a6249ab1f
Minchan Kim <minchan@kernel.org>
2013-08-12 15:13:55 +0900

    update

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -521,7 +521,7 @@ static int zram_bvec_rw(struct zram *zram, struct bio_vec *bvec, u32 index,
 	return ret;
 }
 
-static void zram_reset_device(struct zram *zram)
+static void zram_reset_device(struct zram *zram, bool reset_capacity)
 {
 	size_t index;
 	struct zram_meta *meta;
@@ -547,7 +547,13 @@ static void zram_reset_device(struct zram *zram)
 	memset(&zram->stats, 0, sizeof(zram->stats));
 
 	zram->disksize = 0;
+<<<<<<< HEAD
 	set_capacity(zram->disk, 0);
+=======
+	if (reset_capacity)
+		set_capacity(zram->disk, 0);
+	up_write(&zram->init_lock);
+>>>>>>> 36ba493... zram: fix invalid memory access
 }
 
 static void zram_init_device(struct zram *zram, struct zram_meta *meta)
@@ -630,7 +636,7 @@ static ssize_t reset_store(struct device *dev,
 	if (bdev)
 		fsync_bdev(bdev);
 
-	zram_reset_device(zram);
+	zram_reset_device(zram, true);
 	return len;
 }
 
@@ -717,7 +723,6 @@ error:
 	return 0;
 }
 
-<<<<<<< HEAD
 static void __zram_reset_device(struct zram *zram)
 {
 	size_t index;
@@ -860,10 +865,8 @@ fail:
 }
 
 void zram_slot_free_notify(struct block_device *bdev, unsigned long index)
-=======
 static void zram_slot_free_notify(struct block_device *bdev,
 				unsigned long index)
->>>>>>> 88b3402... zram: remove zram_sysfs file (v2)
 {
 	struct zram *zram;
 
@@ -1044,10 +1047,12 @@ static void __exit zram_exit(void)
 	for (i = 0; i < num_devices; i++) {
 		zram = &zram_devices[i];
 
-		get_disk(zram->disk);
 		destroy_device(zram);
-		zram_reset_device(zram);
-		put_disk(zram->disk);
+		/*
+		 * Shouldn't access zram->disk after destroy_device
+		 * because destroy_device already released zram->disk.
+		 */
+		zram_reset_device(zram, false);
 	}
 
 	unregister_blkdev(zram_major, "zram");
